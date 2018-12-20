074e5b1b8bff3565fdbb9efd5a83809d2e80f4fb
lojpark <lojpark@gmail.com>
2018-12-21 03:13:14 +0900

    fix: move bullets as angle

--- bullet.py
+++ bullet.py
@@ -1,20 +1,45 @@
+import math
+
 class Bullet:
-    def __init__(self, pygame):
+    def __init__(self, pygame, x, y, angle, speed, who):
         # 총알 초기화
-        self.image = pygame.image.load('image\\bullet.png')
+        if who == 'player':
+            self.image = pygame.image.load('image\\bulletA.png')
+        else:
+            self.image = pygame.image.load('image\\bulletB.png')
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+        self.x, self.y, self.angle, self.speed, self.who = x, y, angle, speed, who
+        self.ox, self.oy = self.x, self.y
+        self.r = 0
+        
+    def move(self):
+        # 총알 이동
+        self.x = self.ox + math.cos(self.angle * math.pi / 180) * self.r
+        self.y = self.oy + math.sin(self.angle * math.pi / 180) * self.r
+        self.r += self.speed
+
+    def print(self, screen):
+        # 총알 그리기
+        screen.blit(self.image, (self.x - self.width / 2, self.y - self.height / 2))
+
+class Bullets:
+    def __init__(self, pygame):
+        self.pygame = pygame
         self.bullets = []
 
-    def append(self, x, y):
-        self.bullets.append([x, y])
+    def append(self, x, y, angle, speed, who):
+        self.bullets.append(Bullet(self.pygame, x, y, angle, speed, who))
 
     def move(self):
         # 총알 이동
         for i in reversed(range(0, len(self.bullets))):
-            self.bullets[i][1] -= 10
-            if self.bullets[i][1] < 0:
+            self.bullets[i].move()
+            if self.bullets[i].y < -20 or self.bullets[i].y > 620:
                 del self.bullets[i]
 
     def print(self, screen):
         # 총알 그리기
         for bullet in self.bullets:
-            screen.blit(self.image, (bullet[0] - 8, bullet[1] - 8))
+            bullet.print(screen)
