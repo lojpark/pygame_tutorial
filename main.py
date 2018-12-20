da3a086b512996132d592451949cbbdc502acb7a
lojpark <lojpark@gmail.com>
2018-12-21 03:19:06 +0900

    fix: main process

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from key import *
 
 # 게임 화면 초기화
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Space')
 fps = pygame.time.Clock()
 
@@ -14,7 +14,10 @@ fps = pygame.time.Clock()
 player = Player(pygame)
 
 # 총알 초기화
-bullet = Bullet(pygame)
+bullet = Bullets(pygame)
+
+# 적 초기화
+enemy = Enemies(pygame)
 
 # 키 초기화
 key = Key()
@@ -36,18 +39,38 @@ while is_playing:
         if event.type == pygame.KEYUP:
             key.set_up(pygame, event.key)
 
-    # 플레이어 이동
-    player.move(key)
-    # 총알 이동
-    bullet.move()
+    if player.alive:
+        # 적 추가
+        enemy.append()
 
-    # 화면 지우기
-    screen.fill((0, 0, 0))
+        # 플레이어 이동
+        player.move(key, bullet)
+        # 적 이동
+        enemy.move(bullet, player)
+        # 총알 이동
+        bullet.move()
 
-    # 총알 그리기
-    bullet.print(screen)
-    # 플레이어 그리기
-    player.print(screen)
+        # 플레이어와 적이 충돌
+        player.crash(enemy, bullet)
+        # 적과 총알이 충돌
+        enemy.crash(bullet)
+
+        # 화면 지우기
+        screen.fill((0, 0, 0))
+
+        # 총알 그리기
+        bullet.print(screen)
+        # 플레이어 그리기
+        player.print(screen)
+        # 적 그리기
+        enemy.print(screen)
+        
+    else:
+        textfont = pygame.font.Font('freesansbold.ttf', 80)
+        text = textfont.render("YOU DIED!", True, (255, 0, 0))
+        textpos = text.get_rect()
+        textpos.center = (400, 300)
+        screen.blit(text,textpos)
 
     # 화면 업데이트
     pygame.display.update()
