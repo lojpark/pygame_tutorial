5ce15820eb6ef7ea22c08f19d9764ad2f22cc59e
lojpark <lojpark@gmail.com>
2018-12-21 03:20:29 +0900

    fix: player die when hit by enemy's bullet

--- player.py
+++ player.py
@@ -2,13 +2,18 @@ class Player:
     def __init__(self, pygame):
         # 플레이어 초기화
         self.image = pygame.image.load('image\\player.png')
-        self.x = 320;
-        self.y = 300;
         self.width = self.image.get_width()
         self.height = self.image.get_height()
+        
+        self.x = 400;
+        self.y = 500;
         self.alive = True
+        
+        self.angle = 0
+        self.loaded = 0
+        self.fire_delay = 5
 
-    def move(self, key):
+    def move(self, key, bullet):
         # 플레이어 이동
         if key.up:
             self.y -= 5
@@ -18,10 +23,39 @@ class Player:
             self.x -= 5
         if key.right:
             self.x += 5
+        # 장전된 총 발사
+        if self.loaded > 0:
+            self.fire_delay -= 1
+            if self.fire_delay <= 0:
+                bullet.append(self.x - 5, self.y, -110, 10, 'player')
+                bullet.append(self.x, self.y, -90, 10, 'player')
+                bullet.append(self.x + 5, self.y, -70, 10, 'player')
+                self.angle += 20
+                self.loaded -= 1
+                self.fire_delay = 5
 
-    def crash(self, enemy):
+    def crash(self, enemy, bullet):
         # 플레이어와 적이 충돌
-        return
+        for e in enemy.enemies:
+            up = e.y - e.height / 2
+            down = e.y + e.height / 2
+            left = e.x - e.width / 2
+            right = e.x + e.width / 2
+            if up <= self.y and self.y <= down:
+                if left <= self.x and self.x <= right:
+                    self.alive = False
+                    break
+        # 플레이어와 적 총알이 충돌
+        for b in bullet.bullets:
+            if b.who == 'enemy':
+                up = self.y - self.height / 2
+                down = self.y + self.height / 2
+                left = self.x - self.width / 2
+                right = self.x + self.width / 2
+                if up <= b.y and b.y <= down:
+                    if left <= b.x and b.x <= right:
+                        self.alive = False
+                        break
 
     def print(self, screen):
         # 플레이어 그리기
